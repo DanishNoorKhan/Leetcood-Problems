153489c89cc33edf5d8eee1e2502aa1a8aa8a96b
Danish Noor <danishnoorkhan111@gmail.com>
2026-09-10 09:33:13 +0500

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        int p1 = 0;
-        int p2 = p1+1;
-        while(p1 < nums.size()-1 && p2 <nums.size()){
-            if(nums[p1] != 0){
-                p1++;
-                p2 = p1 + 1;
-            }
-            else if(nums[p2] == 0){
-
-                p2++;}
-            else {
-                swap(nums[p1], nums[p2]);
-                p1++;
-                p2 = p1 + 1;
-            }
-        }
+        int p = 0;
+        
         for(int i=0; i<nums.size(); i++){
-            cout<<nums[i]<<" ";
+            if(nums[i] != 0){
+                swap(nums[p], nums[i]);
+                p++;
+            }
         }
     }
 };
