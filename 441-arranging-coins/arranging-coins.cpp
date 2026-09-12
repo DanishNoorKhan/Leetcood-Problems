a34b02b9bf1394fa22f8d477f832ce3786f569ca
Danish Noor <danishnoorkhan111@gmail.com>
2026-09-12 13:35:23 +0500

    Time: 4 ms (25.58%) | Memory: 8.8 MB (67.75%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        int i=1;
+        while(n >= i){
+            count++;
+            n = n - i;
+            i++;
+        }
+        return count;
+    }
+};
