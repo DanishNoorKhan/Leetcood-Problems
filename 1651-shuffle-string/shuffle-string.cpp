d42542d201c437f4d891bd9f703c5aee3bcf7668
Danish Noor <danishnoorkhan111@gmail.com>
2026-09-17 15:31:03 +0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (96.80%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string new_s = "";
-        int idx = 0;
-        
-        while(idx < s.size()){
-            int next = 0;
-            for(int i=0; i<indices.size(); i++){
-                if(idx == indices[i]){
-
-                    new_s.push_back(s[i]);
-                    idx++;
-                }
-            }
+        string new_s = s;
+        for(int i=0; i<indices.size(); i++){
+            new_s[indices[i]] = s[i];
         }
         return new_s;
     }
